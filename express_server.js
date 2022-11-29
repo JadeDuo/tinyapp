ff084e2a1e269874cb203b8895fbdd7c9c2b9fec
Jade <jade.duong@telus.com>
2022-11-29 14:56:48 -0800

    linted server

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 
 const urlDatabase = {
@@ -23,7 +23,7 @@ app.get("/urls.json", (req, res) => {
 //displays main URL page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 //displays the url creation page
@@ -35,41 +35,41 @@ app.get("/urls/new", (req, res) => {
 //displays the results page after making new shorturl
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
-  res.render("urls_show", templateVars)
+  res.render("urls_show", templateVars);
 });
 
 //creates new short url - on button press
 app.post("/urls", (req, res) => {
-  //we will send a new id here when given a long URL 
+  //we will send a new id here when given a long URL
   let id = generateRandomString(6);
   urlDatabase[id] = req.body.longURL;
-  res.status(200)
+  res.status(200);
   res.redirect(`/urls/${id}`);
 });
 
 //updates the long url - on button press
 app.post("/urls/:id", (req, res) => {
   let newLongURL = req.body.editURL;
-  urlDatabase[req.params.id] = newLongURL
-  res.redirect("back")
-})
+  urlDatabase[req.params.id] = newLongURL;
+  res.redirect("back");
+});
 
 //allows delete button to remove that entry from DB
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
 //allows a user to login using header button
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("back");
-})
+});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("username");
   res.redirect("/urls");
-})
+});
 
 //redirects to long url with u/ID
 app.get("/u/:id", (req, res) => {
@@ -82,9 +82,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const getRandomInt = function (max) {
+const getRandomInt = function(max) {
   return Math.floor(Math.random() * max);
-}
+};
 
 const generateRandomString = function(numDigits) {
   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -93,4 +93,4 @@ const generateRandomString = function(numDigits) {
     str += chars[getRandomInt(chars.length)];
   }
   return str;
-}
+};
