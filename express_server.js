35bb3fe840e8bbb9a4685df8d8967ca2a690ef84
Jade <jade.duong@telus.com>
2022-11-28 22:20:18 -0800

    adding urls.new and added new endpoint to server. added generate random str function to create ID values for future build

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,6 +28,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+console.log(req.body);
+res.send("Ok");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars)
@@ -35,3 +46,16 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const getRandomInt = function (max) {
+  return Math.floor(Math.random() * max);
+}
+
+const generateRandomString = function(numDigits) {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let str = "";
+  for (let i = 0; i < numDigits; i++) {
+    str += chars[getRandomInt(chars.length)];
+  }
+  return str;
+}
