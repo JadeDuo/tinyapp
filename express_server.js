dd6697c9b44499e87bdab92c4f735ac3856210a0
Jade <jade.duong@telus.com>
2022-11-29 10:05:50 -0800

    added delete url method endppoint and added delete buttons into table.

--- express_server.js
+++ express_server.js
@@ -46,6 +46,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars)
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+});
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
