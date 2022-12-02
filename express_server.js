04b3067af989048696ff15f70fa6383d6cbac071
Jade <jade.duong@telus.com>
2022-12-01 22:41:42 -0800

    clean up and refactored code, moved helper functions to seperate doc and added httpify to prevent repetition in code.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
-const { getUserByEmail } = require("./helpers");
+const { getUserByEmail, urlsForUser, generateRandomString, httpify } = require("./helpers");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -16,39 +17,46 @@ app.use(cookieSession({
 }));
 
 const urlDatabase = {
-
-  
+  eOOu4N: {
+    longURL: "http://www.example.com",
+    ownerID: 'HpuWv3',
+  }
 };
-
+//password is asdf if you would like to use this test user
 const userDatabase = {
   HpuWv3: {
     id: 'HpuWv3',
     email: 'asdf@asdf.com',
-    password: '$2a$10$TI/wajXILiX6ri.daOU4Le8..duo.MT3ZcKEJ8b.byS7X7CIn1FTq'
+    password: '$2a$10$TI/wajXILiX6ri.daOU4Le8..duo.MT3ZcKEJ8b.byS7X7CIn1FTq',
   },
 };
 
 const errorList = {
-
-  "ind-login":  "You are not logged in, please login and try again",
+  "ind-login":  "You are not logged in, please login or register and try again",
   "reg-invalid": "Please ensure all fields are filled.",
   "reg-exists": "This email has already been registered, please login instead",
   "log-notfound": "This email is not in our database. Please register instead",
   "log-mismatch": "Password does not match, please try again.",
   "url-notfound": "Hmm, that shortURL was not found, please check your spelling and try again.",
   "url-unowned": "Oops, looks like you don't own that short URL. Please try again.",
-  
-  
-}
-//------GET displays------
-
-//displays users URLs page, redirects to 
-app.get("/urls", (req, res) => {
+};
+//------GET endpoints------
+// redirects root to urls if logged in, and login otherwise
+app.get("/", (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
     return res.redirect("/login");
   }
-  const userURLs = urlsForUser(userID);
+  return res.redirect("/urls");
+});
+
+//displays users URLs page when logged in, else redirects to login
+app.get("/urls", (req, res) => {
+  const userID = req.session.user_id;
+  if (!userID) {
+    return res.redirect("/error/ind-login");
+  }
+  const userURLs = urlsForUser(userID, urlDatabase);
   const templateVars = {
     urls: urlDatabase,
     user: userDatabase[userID],
@@ -66,7 +74,7 @@ app.get("/urls/new", (req, res) => {
   return res.render("urls_new", templateVars);
 });
 
-//displays Register page, redirects to index if logged in
+//displays Register page, redirects to users URLs if logged in
 app.get("/register", (req, res) => {
   const userID = req.session.user_id;
   const templateVars = { user: userDatabase[userID] };
@@ -76,7 +84,7 @@ app.get("/register", (req, res) => {
   return res.render("urls_register", templateVars);
 });
 
-//displays Register page, redirects to index if logged in
+//displays Register page, redirects to users URLS if logged in
 app.get("/login", (req, res) => {
   const userID = req.session.user_id;
   const templateVars = { user: userDatabase[userID] };
@@ -86,14 +94,7 @@ app.get("/login", (req, res) => {
   return res.render("urls_login", templateVars);
 });
 
-// Displays an error page with details on error and how to resolve.
-app.get("/error/:err", (req, res) => {
-  const userID = req.session.user_id;
-  const templateVars = { user: userDatabase[userID], errKey: req.params.err, errorList };
-  return res.render("urls_error", templateVars);
-});
- 
-//displays the results page after making new shorturl
+//displays the results page after making new shortID. redirects to errors if not logged in, url not valid or url not owned.
 app.get("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
@@ -108,29 +109,43 @@ app.get("/urls/:id", (req, res) => {
   }
   return res.render("urls_show", templateVars);
 });
+
+// redirects user to longurl of the given shortID
+app.get("/u/:id", (req, res) => {
+  const urlObj = urlDatabase[req.params.id];
+  if (!urlObj) {
+    return res.redirect("/error/url-notfound");
+  }
+  return res.redirect(urlObj.longURL);
+});
+
+// Displays an error page that gives details on errors encountered
+app.get("/error/:err", (req, res) => {
+  const userID = req.session.user_id;
+  const templateVars = { user: userDatabase[userID], errKey: req.params.err, errorList };
+  return res.render("urls_error", templateVars);
+});
 //---------------POST w button-----------------
 
-//creates new short url - on button press
-//prevents use when not logged in
+//creates new short url, stores in urlDB. redirects to error if not logged in.
+//adds http:// automatically if user doesn't input it
 app.post("/urls", (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
-    return res.redirect("/error/ind-login")
+    return res.redirect("/error/ind-login");
   }
   let url = req.body.longURL;
-  if (url.indexOf("http://") !== 0 || url.indexOf("https://") !== 0) {
-    url = "http://" + url;
-  }
+  url = httpify(url);
   let id = generateRandomString(6);
   urlDatabase[id] = {
     longURL: url,
     ownerID: userID,
   };
-  return res.status(200).redirect(`/urls/${id}`);
+  return res.redirect(`/urls/${id}`);
 });
 
-//updates the long url - on button press
-//returns error paths if user isn't logged in, ID doesn't exist or shortURL is not owned by user.
+//updates the long url value in urlDB.
+//redirects to error paths if user isn't logged in, ID doesn't exist or shortURL is not owned by user.
 app.post("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
@@ -142,22 +157,21 @@ app.post("/urls/:id", (req, res) => {
   if (urlDatabase[req.params.id].ownerID !== userID) {
     return res.redirect("url-unowned");
   }
-  let newLongURL = req.body.editURL;
-  if (newLongURL.indexOf("http://") !== 0 || newLongURL.indexOf("https://") !== 0) {
-    newLongURL = "http://" + newLongURL;
-  }
-  urlDatabase[req.params.id].longURL = newLongURL;
-  return res.redirect("back");
+  let editURL = req.body.editURL;
+  editURL = httpify(editURL);
+  urlDatabase[req.params.id].longURL = editURL;
+  return res.redirect("/urls");
 });
 
-//removes coresponding URL from DB - on button press
+//removes URL from DB
+//redirects to error paths if user isn't logged in, ID doesn't exist or shortURL is not owned by user.
 app.post("/urls/:id/delete", (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
-    return res.redirect("/error/ind-login")
+    return res.redirect("/error/ind-login");
   }
   if (!urlDatabase[req.params.id]) {
-    return res.redirect("/error/url-notfound")
+    return res.redirect("/error/url-notfound");
   }
   if (urlDatabase[req.params.id].ownerID !== userID) {
     return res.redirect("/error/url-unowned");
@@ -167,28 +181,27 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 //creates new user and adds to userDB
+//redirects to error paths if form isn't filled or email exists in userDB
 app.post("/register", (req, res) => {
-  //if user fails to fill out one or more fields
   if (req.body.email === "" || req.body.password === "") {
     return res.redirect("/error/reg-invalid");
   }
-  //if email already exists in userDB
   if (getUserByEmail(req.body.email, userDatabase) !== undefined) {
     return res.redirect("/error/reg-exists");
   }
-  //if user doesn't already exist
   const id = generateRandomString(6);
   userDatabase[id] = {
     id: id,
     email: req.body.email.toLowerCase(),
-    password: bcrypt.hashSync(req.body.password, 10)
+    password: bcrypt.hashSync(req.body.password, 10),
   };
   req.session.user_id = id;
   console.log(userDatabase);
   return res.redirect("/urls");
 });
 
-//logs in and stores user as cookie - on button press
+//logs in existing user and stores user as cookie
+//redirects to error paths if email not found in DB or password mismatch.
 app.post("/login", (req, res) => {
   let user = getUserByEmail(req.body.email, userDatabase);
   if (!user) {
@@ -201,52 +214,13 @@ app.post("/login", (req, res) => {
   return res.redirect("urls");
 });
 
-//logout via header. deletes user_id cookie - on button press
+//logout via header. deletes user_id cookie. redirects to login.
 app.post("/logout", (req, res) => {
   req.session.user_id = null;
   res.redirect("/login");
 });
 
-//redirect functionality to long url with u/ID
-app.get("/u/:id", (req, res) => {
-  const urlObj = urlDatabase[req.params.id];
-  if (!urlObj) {
-    return res.redirect("/error/url-notfound");
-  }
-  return res.redirect(urlObj.longURL);
-});
-
 //listen text when starting server
 app.listen(PORT, () => {
-  console.log(`Tiny app listening on port ${PORT}!`);
+  console.log(`Tiny app reporting for duty on port ${PORT}!`);
 });
-
-//returns the URLs logged in user owns.
-const urlsForUser = function(id) {
-  let userURLs = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key].ownerID === id) {
-      userURLs[key] = {
-        longURL: urlDatabase[key].longURL,
-        ownerID: id,
-      };
-    }
-  }
-  return userURLs;
-};
-
-
-//Randomization helper function for generateRandomString
-const getRandomInt = function(max) {
-  return Math.floor(Math.random() * max);
-};
-
-//Creates userIDs and shortURL IDs. numDigits is 6 for both IDs.
-const generateRandomString = function(numDigits) {
-  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let str = "";
-  for (let i = 0; i < numDigits; i++) {
-    str += chars[getRandomInt(chars.length)];
-  }
-  return str;
-};
--- helpers.js
+++ helpers.js
@@ -8,4 +8,44 @@ const getUserByEmail = function(email, database) {
   return undefined;
 };
 
-module.exports = { getUserByEmail }
+//returns the URLs logged in user owns.
+const urlsForUser = function(id, database) {
+  let userURLs = {};
+  for (let key in database) {
+    if (database[key].ownerID === id) {
+      userURLs[key] = {
+        longURL: database[key].longURL,
+        ownerID: id,
+      };
+    }
+  }
+  return userURLs;
+};
+
+const httpify = function(url) {
+  if (url.indexOf("http://") !== 0 ) {
+    if(url.indexOf("https://") !==0) {
+      url = "http://" + url;
+    }
+  }
+  return url;
+}
+
+
+//Randomization helper function for generateRandomString
+const getRandomInt = function(max) {
+  return Math.floor(Math.random() * max);
+};
+
+//Creates userIDs and shortURL IDs. numDigits is 6 for both IDs.
+const generateRandomString = function(numDigits) {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let str = "";
+  for (let i = 0; i < numDigits; i++) {
+    str += chars[getRandomInt(chars.length)];
+  }
+  return str;
+};
+
+
+module.exports = { getUserByEmail, urlsForUser, getRandomInt, generateRandomString, httpify }
